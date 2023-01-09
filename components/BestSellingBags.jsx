52d458e30cb57e4aa525839c74332392ca6e0b94
aymen.bnnn <aymenbnn.developer@gmail.com>
2023-01-09 17:41:59 +0100

    add waste not

--- components/BestSellingBags.jsx
+++ components/BestSellingBags.jsx
@@ -4,9 +4,10 @@ function BestSellingBags() {
   return (
     <div  className='container mx-auto '>
         <h3 className='text-2xl font-bold my-5'>Best Selling Bags</h3>
-        <div className='w-full h-screen grid grid-cols-3 grid-rows-2'>
-            <div className='col-span-2 row-span-2'>
-              <div className='w-full h-5/6'>
+        <div className='w-full grid grid-cols-3 grid-rows-2'>
+            {/* first */}
+            <div className='col-span-2 row-span-2 h-full'>
+              <div className='w-full'>
                 <img
                 className='h-full w-full' 
                 src="https://cdn.shopify.com/s/files/1/0287/3670/products/9l_sidekick_814-152-378130021.jpg?v=1670270323&width=750" alt="" />
@@ -16,8 +17,9 @@ function BestSellingBags() {
                 <ArrowLongRightIcon className='w-8' />
               </div>
             </div>
-            <div className='row-span-1 bg-slate-800'>
-              <div className='w-full h-5/6'>
+            {/* Second */}
+            <div className='row-span-1 h-full bg-slate-800'>
+              <div className='w-full'>
             <img
                 className='h-full w-full' 
                 src="https://cdn.shopify.com/s/files/1/0287/3670/products/S22_Accessories-228.jpg?v=1672847461&width=750" alt="" />
@@ -25,23 +27,23 @@ function BestSellingBags() {
               <div className='w-full flex justify-between items-center '>
                 <p className='text-xl font-bold'>DeadStock Revolution</p>
                 <ArrowLongRightIcon className='w-8' />
-            </div>
-            <div className='row-span-1 bg-orange-700'>
-            <div className='row-span-1 bg-slate-800'>
-              <div className='w-full h-5/6'>
-            <img
-                className='h-full w-full' 
-                src="https://cdn.shopify.com/s/files/1/0287/3670/products/S22_Accessories-228.jpg?v=1672847461&width=750" alt="" />
-              </div>
-              <div className='w-full flex justify-between items-center '>
-                <p className='text-xl font-bold'>DeadStock Revolution</p>
-                <ArrowLongRightIcon className='w-8' />
-            </div>
             </div>
 
+            </div>
+            {/* third */}
+            <div className='row-span-1 h-full bg-slate-800'>
+              <div className='w-full'>
+            <img
+                className='h-full w-full' 
+                src="https://cdn.shopify.com/s/files/1/0287/3670/products/S22_Accessories-228.jpg?v=1672847461&width=750" alt="" />
+              </div>
+              <div className='w-full flex justify-between items-center '>
+                <p className='text-xl font-bold'>DeadStock Revolution</p>
+                <ArrowLongRightIcon className='w-8' />
+            </div>
+            </div>
+        
         </div>
-    </div>
-    </div>
     </div>
   )
 }
---
+++ components/WasteNot.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function WasteNot() {
+  return (
+    <div className='w-full h-screen relative'>
+        <img className='w-full h-screen absolute top-0 left-0 -z-20 ' 
+        src="//cdn.shopify.com/s/files/1/0287/3670/files/AdobeStock_10198280_2.jpg?v=1661369229&width=3000" alt="" />
+
+        <div className='w-full h-screen flex flex-col justify-center items-center bg-opacity-40 top-0 bg-gray-700'>
+            <h5 className='font-bold w-full text-center text-white text-4xl mb-4'>Waste Not</h5>
+            <p className='text-md font-normal w-full text-center px-5 text-white'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque rerum quos, eius accusamus aperiam voluptatem velit, nesciunt aliquid natus adipisci perspiciatis unde, quae pariatur possimus laborum quaerat ab fugiat veritatis.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default WasteNot
